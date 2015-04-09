92549d5769bbec9a5ba62eea0f3451a44e190bc0
MadhuriShah <madhurishah393@gmail.com>
2015-04-09 15:26:00 -0400

    new heole

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
+var app=express();
 app.get('/', function (req, res) {
 	res.send('hello world');
 });
-
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
