6b48f2096e23fe1fa74e7f9a41d024005ae61b85
MadhuriShah <madhurishah393@gmail.com>
2015-04-10 23:35:25 -0400

    api

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app=express();
-app.use(express.bodyParser());
 var twilio = require('twilio');
-var client = new twilio.RestClient('AC2c6df62a9c6349427f98bddc594df420', '08e1e5aeda14439fa14d137612cd1a5c');
+var client = new twilio.RestClient('AC2c6df62a9c6349427f98bddc594df420', '08e1e5aeda14439fa14d137612cd1a5c');//my twilio account sid and Auth token
 app.get('/', function (req, res) {
-	res.send('hello world');
+	res.send('hello world'); // when no parameters
 });
 app.get('/send', function (req, res) {
-	var To=req.query.To;
-	var From='+12267782157';
-	var Body=req.query.Body;	
+	var To=req.query.To; // A phone number passed from app
+	var From='+12267782157'; // twilio account phone number
+	var Body=req.query.Body; //location passed from app	
 client.sms.messages.create({
    to:To,
    from:From,
@@ -32,7 +31,7 @@ client.calls.create({
 }, function(err, call) { 
 	console.log('Call Success:'+call.sid); 
 }))
-	//res.send('hello '+To+From+Body);
+res.send('hello '+To+From+Body);
 });
 var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
